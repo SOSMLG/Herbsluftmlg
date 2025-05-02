05ae4b14cad9c996beaa457e75b622edaba2504d
SOSMLG <117748270+SOSMLG@users.noreply.github.com>
2025-05-02 15:00:58 +0100

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,9 @@
+sudo apt update && sudo apt install -y \
+    build-essential libfreetype6-dev libxft-dev libxinerama-dev libx11-dev libxcb1-dev \
+    libharfbuzz-dev xserver-xorg xbacklight xbindkeys xvkbd xinit dbus xinput gcc \
+    flameshot make binutils picom git network-manager herbstluftwm \
+    thunar-archive-plugin thunar-volman xarchiver lxappearance dialog mtools avahi-daemon \
+    acpi acpid gvfs xfce4-power-manager pulseaudio pavucontrol pulsemixer feh \
+    papirus-icon-theme exa maim dunst xdotool unzip micro geany geany-plugins redshift \
+    firefox-esr vlc fonts-font-awesome fonts-terminus libnotify-bin sddm \
+    firmware-iwlwifi firmware-misc-nonfree libxcb-res0-dev libx11-xcb-dev \
